2b065bd87281045981788d9b22c5b4ef6e7cd56c
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2021-05-13 19:53:16 -0300

    style: material-ui imports

--- frontend/src/studentPages/ClassMaterial/ClassMaterial.js
+++ frontend/src/studentPages/ClassMaterial/ClassMaterial.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TablePagination from '@material-ui/core/TablePagination';
-import Paper from '@material-ui/core/Paper';
-import Select from '@material-ui/core/Select';
-import { FormControl, InputLabel } from '@material-ui/core';
+import {
+  withStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  Paper,
+  Select,
+  FormControl,
+  InputLabel,
+} from '@material-ui/core';
 
 import download from '../../assets/icons/download.svg';
 
--- frontend/src/studentPages/Frequency/Frequency.js
+++ frontend/src/studentPages/Frequency/Frequency.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import {
+  withStyles,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TablePagination,
+  Paper,
+} from '@material-ui/core';
 
 import styles from './styles.module.css';
 
