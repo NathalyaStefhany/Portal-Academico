42e19e5398a0906eb8c143f7392815e3198b0102
RenanDias12 <52115774+RenanDias12@users.noreply.github.com>
2021-05-21 16:50:37 -0300

    Ordenacao do historico

--- backend/src/services/HistoricService.ts
+++ backend/src/services/HistoricService.ts
@@ -19,6 +19,44 @@ class HistoricService {
       return 0;
     }
 
+    subjects = subjects.sort(
+      function shortSubject(subject1: SubjectHist, subject2: SubjectHist) {
+        let num1 = subject1.SemesterYear.charAt(2) as unknown as number;
+        let num2 = subject2.SemesterYear.charAt(2) as unknown as number;
+
+        if (num1 > num2) {
+          return 1;
+        }
+        else if (num1 < num2) {
+          return -1;
+        }
+        else {
+          num1 = subject1.SemesterYear.charAt(3) as unknown as number;
+          num2 = subject2.SemesterYear.charAt(3) as unknown as number;
+
+          if (num1 > num2) {
+            return 1;
+          }
+          else if (num1 < num2) {
+            return -1;
+          }
+          else {
+            num1 = subject1.SemesterYear.charAt(5) as unknown as number;
+            num2 = subject2.SemesterYear.charAt(5) as unknown as number;
+
+            if (num1 > num2) {
+              return 1;
+            }
+            else if (num1 < num2) {
+              return -1;
+            }
+            else {
+              return 0;
+            }
+          }
+        }
+      });
+
     const historic = new Historic(matriculationNumber, subjects);
 
     await this.historicRepository.insertOne(historic);
