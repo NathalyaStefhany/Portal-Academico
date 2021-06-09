ba524b27435480027d389917cf50e1f2f6848172
RenanDias12 <52115774+RenanDias12@users.noreply.github.com>
2021-06-09 15:18:31 -0300

    Integração de reposições

--- mobile/lib/src/views/replacementView.dart
+++ mobile/lib/src/views/replacementView.dart
@@ -1,7 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mobile/src/core/appColors.dart';
 import 'package:mobile/src/core/appTextStyles.dart';
 import 'package:mobile/src/views/menuView.dart';
+import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
 
 class Replacement {
   final String subject;
@@ -10,36 +15,84 @@ class Replacement {
   Replacement(this.subject, this.date);
 }
 
-class ReplacementView extends StatelessWidget {
-  final List<Replacement> tests = [
-    Replacement('C213', '18/04/2021'),
-    Replacement('C209', '29/04/2021'),
-  ];
+class ReplacementView extends StatefulWidget {
+  final Map<dynamic, dynamic> studentInfo;
+
+  ReplacementView({this.studentInfo});
+
+  @override
+  _ReplacementViewState createState() => _ReplacementViewState();
+}
+
+class _ReplacementViewState extends State<ReplacementView> {
+  List<Replacement> replacements = [];
+  String matriculationNumber;
+
+  getReplacements() async {
+    String url =
+        DotEnv().env['URL'] + "/student/replacements/$matriculationNumber";
+    http.Response response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    List result = await json.decode(response.body);
+    List<Replacement> replacementsOfGet = [];
+
+    result.forEach((rep) {
+      String acronym;
+
+      rep["Class"].toString().isEmpty
+          ? acronym = rep["Acronym"]
+          : acronym = '${rep["Acronym"]} - ${rep["Class"]}';
+
+      String date = DateFormat("dd/MM/yyyy").format(DateTime.parse(rep["Date"].toString()));
+
+      replacementsOfGet.add(new Replacement(acronym, date));
+      
+    });
+
+    setState(() {
+      replacements = replacementsOfGet;
+    });
+  }
+
+  void asyncInit() async {
+    await getReplacements();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    matriculationNumber = widget.studentInfo["matriculationNumber"].toString();
+    asyncInit();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MenuView(),
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: Icon(Icons.menu), 
-            onPressed: (){
-              Scaffold.of(context).openDrawer();
-            }
-          )
+        drawer: MenuView(studentInfo: widget.studentInfo),
+        appBar: AppBar(
+          leading: Builder(
+              builder: (context) => IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  })),
+          title: Text('Reposições'),
+          backgroundColor: AppColors.blue,
         ),
-        title: Text('Reposições'),
-        backgroundColor: AppColors.blue,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: 
-            Container(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Container(
               decoration: BoxDecoration(
                 color: AppColors.white,
-                border: Border.fromBorderSide(BorderSide(color: AppColors.darkBlue, width: 2)),
+                border: Border.fromBorderSide(
+                    BorderSide(color: AppColors.darkBlue, width: 2)),
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Table(
@@ -47,40 +100,40 @@ class ReplacementView extends StatelessWidget {
                 defaultVerticalAlignment: TableCellVerticalAlignment.middle,
                 children: [
                   TableRow(
-                    decoration: BoxDecoration(
-                      border: Border(bottom: BorderSide(color: AppColors.darkBlue, width: 2))
-                    ),
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Text('Turma', style: AppTextStyles.bodyBlue16, textAlign: TextAlign.center)
-                      ),
-                      Text('Data', style: AppTextStyles.bodyBlue16, textAlign: TextAlign.center),
-                    ]
-                  ),
-                  for(var i = 0; i < tests.length; i++)
-                    TableRow(
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(
+                                  color: AppColors.darkBlue, width: 2))),
                       children: [
                         Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          child: Text(
-                            tests[i].subject, 
-                            style: AppTextStyles.body, 
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                        Text(
-                          tests[i].date, 
-                          style: AppTextStyles.body, 
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Text('Turma',
+                                style: AppTextStyles.bodyBlue16,
+                                textAlign: TextAlign.center)),
+                        Text('Data',
+                            style: AppTextStyles.bodyBlue16,
+                            textAlign: TextAlign.center),
+                      ]),
+                  for (var i = 0; i < replacements.length; i++)
+                    TableRow(children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Text(
+                          replacements[i].subject,
+                          style: AppTextStyles.body,
                           textAlign: TextAlign.center,
                         ),
-                      ]
-                    )
+                      ),
+                      Text(
+                        replacements[i].date,
+                        style: AppTextStyles.body,
+                        textAlign: TextAlign.center,
+                      ),
+                    ])
                 ],
               ),
             ),
-        ),
-      )
-    );
+          ),
+        ));
   }
 }
